7b7aa94bc253ae82e468325cdfaaaece543d232b
Yash Kalwani <yash.kalwani@dal.ca>
2023-07-09 16:13:27 -0300

    cloud client

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import ShortenURLForm from "./components/ShortenURLForm";
+
+
+function App() {
+  return (
+    <ShortenURLForm/>
+  );
+}
+
+export default App;
---
+++ src/components/ShortenURLForm.jsx
@@ -0,0 +1,159 @@
+import React, { useState, useRef } from 'react';
+import { TextField, Button, Grid, Box, Container, Snackbar, Typography, Divider } from '@material-ui/core';
+import { Alert as MuiAlert } from '@material-ui/lab'
+import CopyIcon from '@mui/icons-material/ContentCopy';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import ReplaySharpIcon from '@mui/icons-material/ReplaySharp';
+const urlShortnerService = process.env.SHORTNER_SERVICE || "localhost"
+
+const ShortenURLForm = () => {
+    const [longURL, setLongURL] = useState('');
+    const [shortUrl, setShortUrl] = useState('');
+    const textFieldRef = useRef(null);
+
+    const handleCopy = () => {
+        if (textFieldRef.current) {
+            textFieldRef.current.select();
+            navigator.clipboard.writeText(shortUrl);
+            showSnackbar('copied to clipboard!')
+        }
+    };
+
+    const handleReset = () => {
+        setLongURL('')
+        setShortUrl('')
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setShortUrl(longURL)
+        fetch(urlShortnerService, {
+            method: 'POST',
+            body: JSON.stringify({ url: longURL }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                // Display the shortened URL or error message
+                if (data.statusCode === 200) {
+                    setShortUrl(data.body.shortUrl)
+                    showSnackbar(data.body.shortUrl);
+                } else {
+                    showSnackbar('Error: Unable to shorten URL');
+                }
+            })
+            .catch(() => {
+                showSnackbar('Error: Unable to connect to the server');
+            });
+    };
+
+    const [snackbarOpen, setSnackbarOpen] = React.useState(false);
+    const [snackbarMessage, setSnackbarMessage] = React.useState('');
+
+    const showSnackbar = (message) => {
+        setSnackbarMessage(message);
+        setSnackbarOpen(true);
+    };
+
+    const closeSnackbar = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setSnackbarOpen(false);
+    };
+
+    return (
+        <Container maxWidth="sm">
+            <Grid>
+                <Typography variant="h4" component="h1" align="center" gutterBottom>
+                    Cloud Link Shortner
+                </Typography>
+            </Grid>
+            <Divider />
+            <Box component='form' onSubmit={handleSubmit} sx={{
+                '& .MuiTextField-root': { m: 1, width: '25ch' },
+            }}
+                noValidate
+                autoComplete="off">
+                <Grid container spacing={8}>
+
+                    <Grid item xs={10}>
+                        <TextField
+                            multiline
+                            label="Original URL"
+                            id="outlined-multiline-static"
+                            fullWidth
+                            value={longURL}
+                            onChange={(e) => setLongURL(e.target.value)}
+                        />
+                    </Grid>
+                    <Grid item xs={2}>
+                        <Tooltip title="Reset">
+                            <IconButton onClick={handleReset}>
+                                <ReplaySharpIcon />
+                            </IconButton>
+                        </Tooltip>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            fullWidth
+                            align="left"
+                            onSubmit={handleSubmit}
+                        >
+                            Shorten
+                        </Button>
+                    </Grid>
+
+                    {(shortUrl) ?
+                        <>
+                            <Grid item xs={10}>
+                                <TextField
+                                    multiline
+                                    inputRef={textFieldRef}
+                                    label="Short URL"
+                                    id="outlined-multiline-static"
+                                    fullWidth
+                                    disabled
+                                    value={shortUrl}
+                                    onChange={(e) => setLongURL(e.target.value)}
+                                />
+
+                            </Grid>
+                            <Grid item xs={2}>
+                                <Tooltip title="Copy Short URL">
+                                    <IconButton onClick={handleCopy}>
+                                        <CopyIcon />
+                                    </IconButton>
+                                </Tooltip>
+                            </Grid>
+                        </> : <></>
+                    }
+                </Grid>
+            </Box>
+
+            <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={4000}
+                onClose={closeSnackbar}
+            >
+                <MuiAlert
+                    elevation={6}
+                    variant="filled"
+                    onClose={closeSnackbar}
+                    severity="success"
+                >
+                    {snackbarMessage}
+                </MuiAlert>
+            </Snackbar>
+        </Container>
+    );
+};
+
+export default ShortenURLForm;
